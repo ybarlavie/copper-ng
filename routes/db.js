db7b0bd4f10a0ca8d074a4f64f9988fe0dc1e64c
Yoni Bar-Lavie <yonib@infigis.net>
2020-05-18 00:26:56 +0300

    connect to db

---
+++ routes/db.js
@@ -0,0 +1,29 @@
+var MongoClient = require('mongodb');
+var express = require('express');
+
+const uri = "mongodb://root:example@localhost:27017";
+//const uri = "mongodb://localhost:27017";
+
+var router = express.Router();
+
+router.get('/:collection', function (req, res) {
+    console.log(req.params.collection + ' sync');
+
+    MongoClient.connect(uri, function(err, client) {
+        if(err) { return console.dir(err); }
+
+        var db = client.db('copper-db');
+
+        var collection = db.collection(req.params.collection);
+        var filter = {};
+        filter[req.query.qv] = new RegExp(req.query.qe, 'i');
+
+        collection.find(filter).toArray(function(err, items) {
+            return res.status(200).send(items);
+        });
+
+        client.close();
+    });
+});
+
+module.exports = router;
