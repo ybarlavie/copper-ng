bf4d532feeb561440e097d0beb71b22b68c366f0
Yoni Bar-Lavie <yonib@infigis.net>
2020-10-07 01:02:56 +0300

    hebrew....

--- src/components/searchParams.vue
+++ src/components/searchParams.vue
@@ -46,7 +46,7 @@
                     //     value: 'wholeWord'
                     // },
                     {
-                        label: 'התעלם מסוגריים מרובעות',
+                        label: 'התעלם מסוגריים מרובעים',
                         value: 'ignoreSquare'
                     },
                     {
